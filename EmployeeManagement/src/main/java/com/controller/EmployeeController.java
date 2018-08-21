de04c7909744093976db98b36a93b8bbafd0c227
Tejavanth <tejavanth.velivelli@gmail.com>
2018-08-21 17:15:48 -0400

    Added Log4j.properties file

--- EmployeeManagement/src/main/java/com/controller/EmployeeController.java
+++ EmployeeManagement/src/main/java/com/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -17,52 +18,55 @@ import com.services.EmployeeServices;
 
 @Controller
 public class EmployeeController {
-	
+
+	Logger logger = Logger.getLogger(EmployeeController.class);
+
 	@Autowired
 	private EmployeeServices employeeService;
-	
-	@RequestMapping(value="/")
-	public ModelAndView listContact(ModelAndView model) throws IOException{
+
+	@RequestMapping(value = "/")
+	public ModelAndView listContact(ModelAndView model) throws IOException {
 		Set<Employee> employees = employeeService.getAllEmployees();
+		logger.info("Adding Employee Objects");
 		model.addObject("employees", employees);
-		
+
 		model.setViewName("home");
-		
+
 		return model;
 	}
-	
-	
+
 	@RequestMapping(value = "/new", method = RequestMethod.GET)
 	public ModelAndView newContact(ModelAndView model) {
 		Employee employee = new Employee();
-		
+		logger.info("Creating a new Employee");
 		model.addObject("employee", employee);
 		model.setViewName("new");
 		return model;
 	}
-	
+
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
 	public ModelAndView saveContact(@ModelAttribute Employee employee) {
-		employeeService.addEmp(employee);		
+		logger.info("In save method");
+		employeeService.addEmp(employee);
 		return new ModelAndView("redirect:/");
 	}
+
 	@RequestMapping(value = "/update", method = RequestMethod.GET)
 	public ModelAndView updateContact(@ModelAttribute int id, @ModelAttribute Employee employee) {
 		ModelAndView model = new ModelAndView();
-		
+		logger.info("Update an Employee");
 		model.setViewName("update");
-		employeeService.updateEmployee(id,employee);		
+		employeeService.updateEmployee(id, employee);
 		return new ModelAndView("redirect:/");
 	}
-	
+
 	@RequestMapping(value = "/delete")
 	public ModelAndView deleteContact(HttpServletRequest request) {
 		int eid = Integer.parseInt(request.getParameter("eid"));
+		logger.info("Delete an Employee");
+		employeeService.deleteEmployee(eid);
 		
-		employeeService.deleteEmployee(eid);;
 		return new ModelAndView("redirect:/");
 	}
-	
-
 
 }
